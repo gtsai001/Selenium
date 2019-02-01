1a5316fe0e9543f7d0036b09ebc6f02a27de5375
George Tsai <gtsai@Georges-MBP.tx.rr.com>
2019-02-01 15:52:57 -0600

    remove the before comment

--- co/edureka/selenium/deom/Demo.java
+++ co/edureka/selenium/deom/Demo.java
@@ -14,7 +14,6 @@ public class Demo {
 		driver.findElement(By.id("identifierId")).sendKeys("gtsai001@gmail.com");
 		driver.findElement(By.className("CwaK9")).click();
 			
-		//add comment in test 1 before
 		String title = driver.getTitle();
 		driver.close();
 		if ("gmail".equalsIgnoreCase(title))
